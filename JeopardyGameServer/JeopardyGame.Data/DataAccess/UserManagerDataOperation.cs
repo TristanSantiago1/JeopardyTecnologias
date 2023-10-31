f3179b0c4d17fdb617f9b350448e871991899a7a
TristanSantiago1 <tristan.santiago520@gmail.com>
2023-10-31 07:35:34 -0600

    merge ya chdio

--- JeopardyGameServer/JeopardyGame.Data/DataAccess/UserManagerDataOperation.cs
+++ JeopardyGameServer/JeopardyGame.Data/DataAccess/UserManagerDataOperation.cs
@@ -9,7 +9,6 @@ using System.Data.Entity.Core.Metadata.Edm;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Runtime.Remoting.Contexts;
 using System.Security.Cryptography;
@@ -217,25 +216,24 @@ namespace JeopardyGame.Data.DataAccess
 
         public bool VerifyPassword(string password, string hashedPassword)
         {
-            try
+            try { 
+            byte[] hashBytes = Convert.FromBase64String(hashedPassword);
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+            var passBaseKeyDerFun2 = new Rfc2898DeriveBytes(password, salt, 10000, HashAlgorithmName.SHA256);
+            byte[] hash = passBaseKeyDerFun2.GetBytes(20);
+            for (int i = 0; i < 20; i++)
             {
-                byte[] hashBytes = Convert.FromBase64String(hashedPassword);
-                byte[] salt = new byte[16];
-                Array.Copy(hashBytes, 0, salt, 0, 16);
-                var passBaseKeyDerFun2 = new Rfc2898DeriveBytes(password, salt, 10000, HashAlgorithmName.SHA256);
-                byte[] hash = passBaseKeyDerFun2.GetBytes(20);
-                for (int i = 0; i < 20; i++)
-                {
-                    if (hashBytes[i + 16] != hash[i])
-                        return false;
-                }
-
-                return true;
+                if (hashBytes[i + 16] != hash[i])
+                    return false;
             }
-            catch(SqlException ex)
+
+            return true;
+            }
+            catch (SqlException ex)
             {
                 ExceptionHandler.HandleExcpeotion(ex, ExceptionDiccionary.FATAL_EXCEPTION);
-            }
+            }            
             catch (EntityException entityEx)
             {
                 ExceptionHandler.HandleExcpeotion(entityEx, ExceptionDiccionary.FATAL_EXCEPTION);
